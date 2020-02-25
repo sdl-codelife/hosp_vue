0f35a0dfd100e93969cd51d24dd161d98335b596
sdl-codelife <sdllove1314mido@gmail.com>
2020-02-25 19:51:55 +0800

    add some

--- src/views/doctor/SeeRegist.vue
+++ src/views/doctor/SeeRegist.vue
@@ -13,49 +13,92 @@
             <el-input v-model="petname" prefix-icon="el-icon-search" clearable placeholder="宠物名"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary">查询</el-button>
+            <el-button @click="findregist" type="primary">查询</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary">重置</el-button>
+            <el-button @click="reset" type="primary">重置</el-button>
           </el-form-item>
         </el-form>
       </el-col>
       <el-table :data="orderdata" style="width: 100%" border>
         <el-table-column type="index" width="60"></el-table-column>
-        <el-table-column prop="id" width="100" label="单号" sortable></el-table-column>
-        <el-table-column prop="date" label="挂号时间"></el-table-column>
         <el-table-column prop="name" label="宠物姓名"></el-table-column>
-        <el-table-column prop="username" label="主人" sortable></el-table-column>
-        <el-table-column prop="doctorname" label="医生"></el-table-column>
-        <el-table-column prop="question" width="300" label="问题"></el-table-column>
-        <el-table-column width="100" label="操作">
+        <el-table-column prop="id" label="单号" sortable></el-table-column>
+        <el-table-column prop="date" label="挂号时间"></el-table-column>
+        <el-table-column prop="question" label="问题"></el-table-column>
+        <el-table-column width="150" label="操作">
           <template scope="scope">
-            <el-button type="danger" @click="deltemedicine(scope.row)" size="small">查看诊断</el-button>
+            <el-button type="danger" @click="seedetailhandle(scope.row)" size="small">查看详情</el-button>
           </template>
         </el-table-column>
       </el-table>
+      <!--分页 -->
+      <el-pagination
+        background
+        layout="total, prev, pager, next"
+        :total="total"
+        :page-size="pagesize"
+        :page-count="pagecount"
+        :current-page="currentpage"
+        style="margin-top:10px"
+      ></el-pagination>
     </div>
+    <el-dialog title="查看详情" :visible.sync="seedetaildialog">
+      <el-collapse
+      <a>宠物名</a>
+      <a>主人</a>
+      <a>挂号时间</a>
+      <a>问题</a>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-  mounted(){
-      this.getorder()
+  mounted() {
+    this.getorder();
   },
   data() {
     return {
+      seedetaildialog: false,
       petname: "",
-      orderdata:[]
+      orderdata: [],
+      total: 0, //总条数
+      pagesize: 6, //每页显示条数
+      pagecount: 0, //总页数
+      currentpage: 1 //当前页
     };
   },
-  methods:{
-      getorder(){
-        this.$getRequest("/getdoctororderlist?resove=" + "1").then(res => {
-       
+  methods: {
+    getorder() {
+      this.$getRequest(
+        "/getregistcomp?" +
+          "pageNo=" +
+          this.currentpage +
+          "&pageSize=" +
+          this.pagesize +
+          "&name=" +
+          this.petname
+      ).then(res => {
         this.orderdata = res.data.data.list;
+        this.total = res.data.data.total; //总条数
+        this.pagesize = res.data.data.pageSize; //每页显示条数
+        this.pagecount = res.data.data.pages; //总页数
+        this.currentpage = res.data.data.pageNum; //当前页数
       });
-      }
+    },
+    findregist() {
+      this.getorder();
+    },
+    reset() {
+      this.petname = "";
+      this.getorder();
+    },
+    seedetailhandle(row) {
+      console.log("diao");
+      
+      this.seedetaildialog = true;
+    }
   }
 };
 </script>
